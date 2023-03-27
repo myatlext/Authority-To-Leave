dc64bc78753d61292609d3919a067088eccc4c17
Tom <thomas.zola@gmail.com>
2023-03-27 21:33:57 +1030

    Minor refactor of repeated code

--- Plugin/AddExtensionAttributes.php
+++ Plugin/AddExtensionAttributes.php
@@ -39,16 +39,24 @@ class AddExtensionAttributes
         return $result;
     }
 
+    /**
+     * @param OrderRepositoryInterface $subject
+     * @param OrderSearchResultInterface $result
+     * @param $id
+     * @return OrderSearchResultInterface
+     */
     public function afterGetList(OrderRepositoryInterface $subject, OrderSearchResultInterface $result, $id): OrderSearchResultInterface
     {
         foreach ($result->getItems() as $order) {
-            $orderExtension = $extensionAttributes ?? $this->orderExtensionFactory->create();
-            $orderExtension->setData('has_authority_to_leave', $this->hasAuthorityToLeave->execute($order));
-            $order->setExtensionAttributes($orderExtension);
+            $this->setExtensionAttributes($order);
         }
         return $result;
     }
 
+    /**
+     * @param OrderInterface $order
+     * @return void
+     */
     private function setExtensionAttributes(\Magento\Sales\Api\Data\OrderInterface $order) : void
     {
         $extensionAttributes = $order->getExtensionAttributes();
